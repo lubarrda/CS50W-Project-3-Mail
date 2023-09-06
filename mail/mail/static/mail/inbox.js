5f2913f76c328de7d6d8352cf85e184f4fbe7897
Luis B <luis@Luiss-MacBook-Air.local>
2023-09-06 07:04:46 +0200

    Final_V2

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -11,16 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Send email
   document.querySelector("#compose-form").addEventListener('submit', send_email);
-
 });
 
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#emails-link-view').style.display = 'none';
-
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -28,8 +25,7 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
-
-function link_email(id) {
+function link_email(id, current_view) {
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
@@ -42,13 +38,13 @@ function link_email(id) {
 
     document.querySelector('#emails-link-view').innerHTML = `
     <ul class="list-group">
-    <li class="list-group-item"><strong>From:</strong> ${email.sender} </li>
-    <li class="list-group-item"><strong>To:</strong> ${email.recipients}</li>
-    <li class="list-group-item"><strong>Subject:</strong> ${email.subject}</li>
-    <li class="list-group-item"><strong>Timestamp:</strong> ${email.timestamp}</li>
-    <li class="list-group-item">${email.body}</li>
-  </ul>
-  `
+      <li class="list-group-item"><strong>From:</strong> ${email.sender}</li>
+      <li class="list-group-item"><strong>To:</strong> ${email.recipients}</li>
+      <li class="list-group-item"><strong>Subject:</strong> ${email.subject}</li>
+      <li class="list-group-item"><strong>Timestamp:</strong> ${email.timestamp}</li>
+      <li class="list-group-item">${email.body}</li>
+    </ul>
+    `
 
     // Email read
     if (!email.read) {
@@ -60,21 +56,22 @@ function link_email(id) {
       })
     }
 
-    // Archive vs Unarchive
-    const btn_arcv = document.createElement('button');
-    btn_arcv.innerHTML = email.archived ? "Unarchive" : "Archive";
-    btn_arcv.className = email.archived ? "btn btn-secondary" : "btn btn-warning";
-    btn_arcv.addEventListener('click', function() {
-      fetch(`/emails/${email.id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
+    if (current_view !== 'sent') {
+      // Archive vs Unarchive
+      const btn_arcv = document.createElement('button');
+      btn_arcv.innerHTML = email.archived ? "Unarchive" : "Archive";
+      btn_arcv.className = email.archived ? "btn btn-secondary" : "btn btn-warning";
+      btn_arcv.addEventListener('click', function() {
+        fetch(`/emails/${email.id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
             archived: !email.archived
+          })
         })
-      })
-      .then(() => {load_mailbox ('inbox')})      
-    });
-    document.querySelector('#emails-link-view').append(btn_arcv);
-    
+        .then(() => { load_mailbox('inbox') })      
+      });
+      document.querySelector('#emails-link-view').append(btn_arcv);
+    }
 
     // Reply
     const btn_reply = document.createElement('button');
@@ -85,81 +82,71 @@ function link_email(id) {
 
       document.querySelector('#compose-recipients').value = email.sender;
       let subject = email.subject;
-      if(subject.split(' ',1)[0] != "Re:") {
+      if (subject.split(' ', 1)[0] != "Re:") {
         subject = "Re: " + email.subject;
       }
       document.querySelector('#compose-subject').value = subject;
       document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
-
     });
     document.querySelector('#emails-link-view').append(btn_reply);
   });
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#emails-link-view').style.display = 'none';
-
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   // pulling emails
   fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
-      // Print emails
-      emails.forEach(sentEmail => {
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    emails.forEach(sentEmail => {
+      console.log(sentEmail);
 
-        console.log(sentEmail);
+      const newEmail = document.createElement('div');
+      newEmail.className = "list-group-item";
+      newEmail.innerHTML = `
+        <h2> Sender: ${sentEmail.sender} </h2>
+        <h4> Subject: ${sentEmail.subject} </h4>
+        <h3>  ${sentEmail.timestamp} </h3>
+      `;
 
-
-        const newEmail = document.createElement('div');
-        newEmail.className = "list-group-item";
-        newEmail.innerHTML = `
-        
-          <h2> Sender: ${sentEmail.sender} </h2>
-          <h4> Subject: ${sentEmail.subject} </h4>
-          <h3>  ${sentEmail.timestamp} </h3>
-        `;
-
-       
-        newEmail.className = sentEmail.read ? 'read' : 'unread';
-        newEmail.addEventListener('click', function() {
-          link_email(sentEmail.id)
-        });
-        document.querySelector('#emails-view').append(newEmail);
+      newEmail.className = sentEmail.read ? 'read' : 'unread';
+      newEmail.addEventListener('click', function() {
+        link_email(sentEmail.id, mailbox)
       });
-      // ... do something else with emails ... adding a nice touch
-      
+      document.querySelector('#emails-view').append(newEmail);
     });
+  });
 }
 
-function send_email(event){
+function send_email(event) {
   event.preventDefault();
 
   // pulling data
   const recipientes = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
-  
+
   // pushing data > BE from CS50
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-        recipients: recipientes,
-        subject: subject,
-        body: body,
+      recipients: recipientes,
+      subject: subject,
+      body: body,
     })
   })
   .then(response => response.json())
   .then(result => {
-      // Print result
-      console.log(result);
-      load_mailbox('sent');
+    // Print result
+    console.log(result);
+    load_mailbox('sent');
   });
-
 }
